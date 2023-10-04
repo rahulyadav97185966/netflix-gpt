d9bb5e77dc3f416c73635ab2fc4e98ff5adbfd80
Sagar Yadav <rahulyadav97185966@gmail.com>
2023-10-04 15:29:42 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-import "./App.css";
-import Login from "./components/Login";
+import Body from "./components/Body";
 
 function App() {
-  return (
-    <>
-      <Login />
-    </>
-  );
+  return <Body />;
 }
 
 export default App;
---
+++ src/components/Body.js
@@ -0,0 +1,12 @@
+import Login from "./Login";
+import Browse from "./Browse";
+const Body = () => {
+  return (
+    <div>
+      <Login />
+      <Browse />
+    </div>
+  );
+};
+
+export default Body;
