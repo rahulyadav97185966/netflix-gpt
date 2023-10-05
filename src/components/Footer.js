046533c482e784904c567b7d540f2045ebeefc9f
Sagar Yadav <rahulyadav97185966@gmail.com>
2023-10-05 21:14:34 +0530

    Code Changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="absolute w-full px-8 my-auto bottom-0 h-96 bg-black bg-opacity-80">
+    <div className="absolute -bottom-32 h-48 w-full bg-black opacity-80">
       Footer
     </div>
   );
--- src/components/Login.js
+++ src/components/Login.js
@@ -8,11 +8,11 @@ const Login = () => {
     <div>
       <Header />
       <img
-        className="absolute"
+        className="absolute object-fill"
         src="https://assets.nflxext.com/ffe/siteui/vlv3/9db4a880-3034-4e98-bdea-5d983e86bf52/b5953637-091d-4e02-9754-2bfadc8a8f7c/IN-en-20230925-popsignuptwoweeks-perspective_alpha_website_large.jpg"
         alt="bg-img"
       />
-      <form className="absolute my-20 w-3/12 p-8 pb-32 px-8 mx-auto right-0 left-0 bg-black bg-opacity-80">
+      <form className="absolute mt-20 w-3/12 p-8 pb-16 px-8 mx-auto right-0 left-0 bg-black bg-opacity-80">
         <h1 className="text-white text-bold text-3xl font-semibold my-4">
           Sign In
         </h1>
